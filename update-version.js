a3ee8e8283114bb9f7cb23dee9c0760317d8f592
Maxim Litvak <maxim.litvak@gmail.com>
2024-03-28 22:06:55 +0100

    output more vars to gitlab runner logs

--- update-version.js
+++ update-version.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const packageJsonPath = './package.json';
 
 // Get the version from GitLab CI environment variable or use a default
+console.log('CI_COMMIT_TAG:', process.env.CI_COMMIT_TAG);
 const version = process.env.CI_COMMIT_TAG || '0.0.0-dev';
 
 // Read package.json
@@ -12,4 +13,10 @@ const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 packageJson.version = version;
 
 // Write package.json back to disk
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
+const pkgContent = JSON.stringify(packageJson, null, 2) + '\n';
+// output content to console
+console.log("content of package.json:", pkgContent);
+// same for the path:
+console.log("path to package.json", packageJsonPath);
+
+fs.writeFileSync(packageJsonPath, pkgContent);
